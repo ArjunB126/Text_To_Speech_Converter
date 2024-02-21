2875256bca5f5873e089b0822f33359ba39588ec
ARJUN BAJARE <128606518+ArjunB126@users.noreply.github.com>
2024-02-21 23:16:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ let voiceSelect = document.querySelector("select");
 
 
 
+
+
+
+
+
 // This block of code sets up an event listener for the onvoiceschanged event of the speechSynthesis object. When this event is triggered, the getVoices() method is called to get the updated list of available voices, which are then stored in the voices array. The first voice in the array is set as the default voice for the speech object.
 
 window.speechSynthesis.onvoiceschanged = () => {
@@ -39,6 +44,11 @@ document.querySelector("button").addEventListener("click", () => {
   speech.text = document.getElementById("textarea").value;
   window.speechSynthesis.speak(speech);
 });
+
+
+
+
+
 
 
 
